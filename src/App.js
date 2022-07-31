84b7981ee5945adf208f5bcf4bfa99f8edb92406
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-31 20:04:44 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-http-realistic--project-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
